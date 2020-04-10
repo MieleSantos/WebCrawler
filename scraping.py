64a76141c9dbf7b468cf22d596bb93cd39299e6f
mielesantos <mielesnts@gmail.com>
2020-04-10 16:26:59 -0300

    atualizando repositorio

--- scraping.py
+++
@@ -1,8 +0,0 @@
-#Baixando arquivos e ,amipulando datas/web scraping
-
-from requests import get
-
-x =get('http://www.google.com')
-
-print(x.headers)
-
--- selleniun2.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-
-vega = webdriver.Firefox()
-vega.get('https://www.kixeye.com/forum/discussion/715463')
